5ee6c3a65dd8a7d0bb1c81edbd60cad4936202ab
rahulsavant <123155143+rahulsavant@users.noreply.github.com>
2023-06-24 13:58:28 +0530

    Add files via upload

---
+++ src/main/java/com/PaymentGateway/PaymentGatewayWithRazorPayApplication.java
@@ -0,0 +1,13 @@
+package com.PaymentGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentGatewayWithRazorPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentGatewayWithRazorPayApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/PaymentGateway/controller/PaymentController.java
@@ -0,0 +1,31 @@
+package com.PaymentGateway.controller;
+
+import com.PaymentGateway.service.PaymentService;
+import com.razorpay.RazorpayException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @Autowired
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping("/create-order")
+    public String createOrder(@RequestParam("amount") int amount, @RequestParam("currency") String currency) throws RazorpayException {
+        String orderId = paymentService.createOrder(amount, currency);
+        return "Order created with ID: " + orderId;
+    }
+
+    @PostMapping("/verify-signature")
+    public String verifyPaymentSignature(@RequestParam String orderId, @RequestParam String paymentId, @RequestParam String signature) throws RazorpayException {
+        return paymentService.verifyPaymentSignature(orderId, paymentId, signature);
+    }
+
+    // Other controller methods for payment callbacks, etc.
+}
